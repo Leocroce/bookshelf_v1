db3d2274878daccec839c5996e9655c05a75b59b
Lucianocbarbosa <lucianobebarbosa@gmail.com>
2022-01-28 15:50:02 -0300

    Endpoint de Artes

---
+++ src/app/modelosInterface/artes.ts
@@ -0,0 +1,11 @@
+export interface Artes {
+
+  titulo: string;
+  autor: string;
+  sinopse: string;
+  img: string;
+  link: [{ ref: string, texto: string }];
+  cols: number;
+  rows: number;
+}
+
