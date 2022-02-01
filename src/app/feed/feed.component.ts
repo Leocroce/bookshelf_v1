d6f7b2da28854293963113c39949b91a04754df5
Lucianocbarbosa <lucianobebarbosa@gmail.com>
2022-02-01 17:15:25 -0300

    Rotina de busca pelo titulo do cartão do Feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,12 +1,13 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, debounceTime, distinctUntilChanged, filter, fromEvent, Observable, of, tap } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { AppDialogosComponent } from '../app-compartilhado/app-dialogos/app-dialogos.component';
+import { Dashboard } from './../modelosInterface/dashboard';
 import { AutenticacaoFirebaseService } from './../servicosInterface/autenticacao-firebase.service';
 import { DashboardService } from './../servicosInterface/dashboard.service';
-import { Dashboard } from './../modelosInterface/dashboard';
-import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-import { Observable, catchError, of } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
-import { AppDialogosComponent } from '../app-compartilhado/app-dialogos/app-dialogos.component';
 
 
 @Component({
@@ -15,8 +16,9 @@ import { AppDialogosComponent } from '../app-compartilhado/app-dialogos/app-dial
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent {
+  resultado$!: Observable<Dashboard | undefined>;
   cards$: Observable<Dashboard[]>;
-  usuario$= this.autenticacaoFirebaseService.usuarioLogado$;
+  usuario$ = this.autenticacaoFirebaseService.usuarioLogado$;
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
@@ -26,24 +28,44 @@ export class FeedComponent {
     })
   );
 
+
+  @ViewChild('searchInput') searchInput!: ElementRef;
+
   constructor(
     private breakpointObserver: BreakpointObserver,
     private dashboardService: DashboardService,
     private autenticacaoFirebaseService: AutenticacaoFirebaseService,
     public dialogo: MatDialog
-    ) {
-      this.cards$ = dashboardService.listagemCards()
+  ) {
+    this.cards$ = dashboardService.listagemCards()
       .pipe(
-        catchError(error =>{
-          this.abrirDialogoErro("Erro ao carregar o feed: #BS -"+error.status)
+        catchError(error => {
+          this.abrirDialogoErro("Erro ao carregar o feed: #BS -" + error.status)
           return of([])
         })
       )
-    }
+  }
 
-    abrirDialogoErro(erroMsg: string){
-      this.dialogo.open(AppDialogosComponent,{
-        data: erroMsg
-      })
-    }
+  abrirDialogoErro(erroMsg: string) {
+    this.dialogo.open(AppDialogosComponent, {
+      data: erroMsg
+    })
+  }
+
+  ngAfterViewInit(): void {
+    fromEvent(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+        filter(Boolean),
+        debounceTime(400),
+        distinctUntilChanged(),
+        tap(() => {
+          const query = this.searchInput.nativeElement.value;
+          if (query) {
+            this.resultado$ = this.dashboardService.pesquisarFeed(query);
+          }
+        })
+      )
+      .subscribe(console.log);
+  }
+
 }
