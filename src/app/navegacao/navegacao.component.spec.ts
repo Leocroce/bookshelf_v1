8826cc7b19661cc62e2046f8abf8af4800c0f99d
ludyklima <ludy.k.lima@gmail.com>
2022-02-02 11:04:57 -0300

    Implementação tooltips nos buttons mais utilizados

--- src/app/navegacao/navegacao.component.spec.ts
+++ src/app/navegacao/navegacao.component.spec.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { NavegacaoComponent } from './navegacao.component';
 
@@ -24,6 +25,7 @@ describe('NavegacaoComponent', () => {
         MatListModule,
         MatSidenavModule,
         MatToolbarModule,
+        MatTooltipModule,
       ]
     }).compileComponents();
   }));
